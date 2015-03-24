b87c58edb223d6f238d45135d8ea91794742df02
Isidro Brizuela <isaubripi@gmail.com>
2015-03-24 19:34:42 -0400

    programa agregado para tal cosa

---
+++ programa.py
@@ -0,0 +1,19 @@
+__author__ = 'isidro'
+
+# coding=utf-8
+#from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You’re at the poll index.") #este comentario fue agregado
+
+def detail(request, poll_id):
+    return HttpResponse("You’re looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You’re looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You’re voting on poll %s." % poll_id)
